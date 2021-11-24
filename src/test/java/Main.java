aa5af73da2a1a235584e36e26709dc84ed643a8f
Yahor <egor.m5252@gmail.com>
2021-11-25 02:01:12 +0300

    add some more random values

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -11,12 +11,17 @@ public class Main { public static void main(String[] args) {
         String c = RandomStringUtils.randomAlphabetic(10);
         String d = RandomStringUtils.randomAlphabetic(10);
         String e = RandomStringUtils.randomAlphabetic(10);
-        String[] arrayRandom = new String[]{a, b, c, d, e};
+        String f = RandomStringUtils.randomAlphabetic(10);
+        String g = RandomStringUtils.randomAlphabetic(10);
+        String h = RandomStringUtils.randomAlphabetic(10);
+        String j = RandomStringUtils.randomAlphabetic(10);
+        String i = RandomStringUtils.randomAlphabetic(10);
+        String[] arrayRandom = new String[]{a, b, c, d, e,f,g,h,j,i};
 
         for (String s : arrayRandom) {
-            System.out.println(s);
+            System.out.println("рандомно сгенеренная стринга :" + s);
             String r = s.substring(0,1).toUpperCase()+s.substring(1).toLowerCase();
-            System.out.println("печать укороченного  :" + r);
+            System.out.println("отформатированная стринга    :" + r);
 
         }
     }
